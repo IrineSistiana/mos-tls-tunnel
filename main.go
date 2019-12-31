c8b1343467aad80158f1310da97c89707f91f149
IrineSistiana <49315432+IrineSistiana@users.noreply.github.com>
2019-12-31 15:32:47 +0800

    set default buff size from 32 -> 4

--- main.go
+++ main.go
@@ -42,7 +42,7 @@ var (
 	certFile           = flag.String("cert", "", "Path to cert, used by server mode. If both key and cert is empty, a self signed certificate will be used")
 	serverName         = flag.String("n", "", "Server name, used to verify the hostname. It is also included in the client's TLS and WSS handshake to support virtual hosting unless it is an IP address.")
 	insecureSkipVerify = flag.Bool("sv", false, "Skip verify, client won't verify the server's certificate chain and host name. In this mode, your connections are susceptible to man-in-the-middle attacks. Use it with caution.")
-	buffSize           = flag.Int("buff", 32, "Size of io buffer for each connection (kb)")
+	buffSize           = flag.Int("buff", 4, "Size of io buffer for each connection (kb)")
 	timeout            = flag.Duration("timeout", 5*time.Minute, "the idle timeout for connections (sec)")
 
 	buffPool   *sync.Pool
